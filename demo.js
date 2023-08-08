d9e075dd9a4bdca6500f2caac7fbd3f8c3627168
axxomano <manocools@gmail.com>
2023-08-08 21:13:24 +0530

    get started with DOM 3/17

--- demo.js
+++ demo.js
@@ -1,1 +1,7 @@
 console.log('I Love GIT!');
+
+document.getElementById('main-header').style.border = "4px solid black"
+
+document.querySelector("#main > h2:nth-child(1)").style.color = 'green'
+
+document.querySelector("#main > h2:nth-child(1)").style.fontWeight = 'bold'
