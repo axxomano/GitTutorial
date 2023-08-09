c282fb4b26ba1764c43c90616a40444c1962cd5f
axxomano <manocools@gmail.com>
2023-08-09 19:08:05 +0530

    Assignment 5 - 7/17

--- demo.js
+++ demo.js
@@ -34,6 +34,41 @@ for(let i=0;i<odd.length;i++){
     odd[i].style.backgroundColor = "green"
 }
 
+let itemList = document.querySelector("#items")
+itemList.parentElement.style.backgroundColor = "#f2f2f2"
+itemList.parentElement.style.backgroundColor = "#f2f2f2"
+
+console.log(itemList.firstChild)//give the first text node// useless
+console.log(itemList.firstElementChild)//give the first element
+console.log(itemList.lastChild)//give the last text node
+console.log(itemList.lastElementChild)//give the last element
+
+console.log(itemList.nextSibling)//give the next sibling text node
+console.log(itemList.nextElementSibling)//give the next sibling to the element
+console.log(itemList.previousSibling)//give the prev sibling text node
+console.log(itemList.previousElementSibling)//give the prev sibling to the element
+
+//Create Element
+
+//Create a div 
+
+let newDiv = document.createElement('div')
+newDiv.className = "Hello"
+newDiv.id = "hello1"
+newDiv.setAttribute("title", "Hello Div")
+
+//create a text node
+let newDivText = document.createTextNode('Hello World!')
+
+//Add text to Div
+newDiv.appendChild(newDivText)
+
+console.log(newDiv)
+
+var container = document.querySelector('header .container')
+var h1 = document.querySelector('header h1')
+
+container.insertBefore(newDiv,h1)
 
 
 
