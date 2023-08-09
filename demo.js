7759ac55c8bcb9f8fa6a92cd09a7f135a49b3f61
axxomano <manocools@gmail.com>
2023-08-09 06:50:39 +0530

    second Assignment

--- demo.js
+++ demo.js
@@ -5,3 +5,11 @@ document.getElementById('main-header').style.border = "4px solid black"
 document.querySelector("#main > h2:nth-child(1)").style.color = 'green'
 
 document.querySelector("#main > h2:nth-child(1)").style.fontWeight = 'bold'
+
+document.querySelector("#items > li:nth-child(3)").style.backgroundColor = "green"
+
+let elems = document.getElementsByClassName('list-group-item')
+
+for(let i=0;i<elems.length;i++){
+    elems[i].style.fontWeight = "bold"
+}
