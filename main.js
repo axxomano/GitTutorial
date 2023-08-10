30faea69fdfa89e517159adba273765e1bea8ab9
axxomano <manocools@gmail.com>
2023-08-10 21:19:04 +0530

    Assignment 13/17

--- main.js
+++ main.js
@@ -14,13 +14,27 @@ document.getElementsByClassName('btn')[0].addEventListener('click',function(){
         'email':email
     }
 
-    
+
     userObj = JSON.stringify(userObj)
 
    //if(!localStorage.getItem('userObj'))
    let userObjItem = 'userObj'+i
    console.log(userObjItem)
         localStorage.setItem(userObjItem, userObj);
+        let li = document.createElement('li')
+        li.id = userObjItem
+        li.innerText = 'Name:' + JSON.parse(userObj).name + 'Email:' + JSON.parse(userObj).email +''
+        let del = document.createElement('button')
+        del.innerText = 'Delete'
+        del.className = 'del'
+        li.appendChild(del)
+        document.getElementById('users').append(li)
+        del.addEventListener('click',function(){
+            //console.log('deleted')
+            localStorage.removeItem(userObjItem)
+            this.parentElement.remove();
+        })
     i++
 })
+
 //.addEventListener('click')
