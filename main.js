e4af4a8af2a034af8ee426040a9580768cca9e5c
Mano Eka <manocools@gmail.com>
2023-08-21 11:13:32 +0000

    Get the saved User Details from crudcrud.

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
-let storedUserObj = localStorage.getItem('userObj')
 
-if(storedUserObj){
-    console.log(JSON.parse(storedUserObj))
-}
+window.addEventListener("DOMContentLoaded",()=>{
+    //let storedUserObj = localStorage.getItem('userObj')
+
+    axios.get("https://crudcrud.com/api/175ff33617f54bb195572008f1b4306d/appointmentData/").then(
+        (resp)=>{
+            console.log(resp)
+            for(let i=0;i<resp.data.length;i++){
+                showUseronScreen(resp.data[i].name,resp.data[i].role)
+            }
+        }).catch((err)=>{
+            console.log(err)
+        })
+})
+
+//if(storedUserObj){
+//    console.log(JSON.parse(storedUserObj))
+//}
 var i = 0
 
 document.getElementsByClassName('btn')[0].addEventListener('click',function(){
@@ -17,46 +30,56 @@ document.getElementsByClassName('btn')[0].addEventListener('click',function(){
 
     userObj = JSON.stringify(userObj)
 
+    axios.post("https://crudcrud.com/api/175ff33617f54bb195572008f1b4306d/appointmentData/",userObj).then((response)=>{
+        console.log(response)
+        showUseronScreen(userObj.name,userObj.email)
+    }).catch((err)=>{
+        console.log(err)
+    })
+
+    
    //if(!localStorage.getItem('userObj'))
    let userObjItem = 'userObj'+i
    console.log(userObjItem)
         //localStorage.setItem(userObjItem, userObj);
-        axios.post("https://crudcrud.com/api/175ff33617f54bb195572008f1b4306d/appointmentData/",userObj).then((response)=>{
-            console.log(response)
-        }).catch((err)=>{
-            console.log(err)
-        })
-
-        let li = document.createElement('li')
-        li.id = userObjItem
-        li.innerText = 'Name:' + JSON.parse(userObj).name + 'Email:' + JSON.parse(userObj).email +''
-        let del = document.createElement('button')
-        let edit = document.createElement('button')
-        edit.innerText = 'Edit'
-        del.innerText = 'Delete'
-        edit.className = 'edit'
-        del.className = 'del'
-        li.appendChild(del)
-        li.appendChild(edit)
-        document.getElementById('users').append(li)
-        del.addEventListener('click',function(){
-            //console.log('deleted')
-            localStorage.removeItem(userObjItem)
-            this.parentElement.remove();
-        })
-        edit.addEventListener('click',function(){
-            //console.log('deleted')
-            let theItem = JSON.parse(localStorage.getItem(userObjItem))
-            let theUser = theItem.name
-            let theEmail = theItem.email
-
-            document.getElementById('name').value = theUser
-            document.getElementById('email').value = theEmail
-            userObjItem.email
-            localStorage.removeItem(userObjItem)
-            this.parentElement.remove();
-        })
+  
     i++
 })
 
 //.addEventListener('click')
+
+function showUseronScreen(name,email,userObjItem){
+    let li = document.createElement('li')
+    li.id = userObjItem
+    li.innerText = 'Name:' + name + 'Email:' + email+''
+    let del = document.createElement('button')
+    let edit = document.createElement('button')
+    edit.innerText = 'Edit'
+    del.innerText = 'Delete'
+    edit.className = 'edit'
+    del.className = 'del'
+    li.appendChild(del)
+    li.appendChild(edit)
+    document.getElementById('users').append(li)
+    del.addEventListener('click',function(){
+        //console.log('deleted')
+        //localStorage.removeItem(userObjItem)
+        //sxios delete
+
+        this.parentElement.remove();
+    })
+    edit.addEventListener('click',function(){
+        //console.log('deleted')
+        //let theItem = JSON.parse(localStorage.getItem(userObjItem))
+        //axios get
+
+        let theUser = theItem.name
+        let theEmail = theItem.email
+
+        document.getElementById('name').value = theUser
+        document.getElementById('email').value = theEmail
+        userObjItem.email
+        localStorage.removeItem(userObjItem)
+        this.parentElement.remove();
+    })
+}
